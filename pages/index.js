9d65da059ecee14515b5af71a102dd0511093502
tinogomes <tinorj@gmail.com>
2020-11-09 15:44:03 -0300

    limpando

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,6 @@ function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <Contador />
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
         </div>
     )
 }
